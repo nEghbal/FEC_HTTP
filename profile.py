3a00388dc9847328c4dc4e044518c4c205e0c5d6
NooshinEghbal <eghbal@ualberta.ca>
2022-04-24 07:40:06 +0100

    Update profile.py

--- profile.py
+++ profile.py
@@ -10,10 +10,13 @@ pc = portal.Context()
 request = pc.makeRequestRSpec()
  
 # Add a raw PC to the request.
-node = request.RawPC("node")
+node1 = request.RawPC("node1")
+
+node2 = request.RawPC("node2")
 
 # Install and execute a script that is contained in the repository.
-node.addService(pg.Execute(shell="sh", command="/local/repository/client-server.sh"))
+node1.addService(pg.Execute(shell="sh", command="/local/repository/server.sh"))
+node2.addService(pg.Execute(shell="sh", command="/local/repository/client.sh"))
 
 # Print the RSpec to the enclosing page.
 pc.printRequestRSpec(request)
