85cd1ff3d62f3eea49c8f093e550fdf4378a0cac
nEghbal <108611559+nEghbal@users.noreply.github.com>
2023-03-05 18:41:37 +0000

    Update profile.py

--- profile.py
+++ profile.py
@@ -11,21 +11,29 @@ request = pc.makeRequestRSpec()
  
 # Add a raw PC to the request.
 node1 = request.RawPC("node1")
-img1 = "urn:publicid:IDN+emulab.net+image+FEC-HTTP:FECHTTP.node1"
-node1.disk_image = img1
+#img1 = "urn:publicid:IDN+emulab.net+image+FEC-HTTP:FECHTTP.node1"
+#node1.disk_image = img1
 node1.routable_control_ip = True
 
 node2 = request.RawPC("node2")
-img2 = "urn:publicid:IDN+emulab.net+image+FEC-HTTP:FECHTTP.node2"
-node2.disk_image = img2
+#img2 = "urn:publicid:IDN+emulab.net+image+FEC-HTTP:FECHTTP.node2"
+#node2.disk_image = img2
 node2.routable_control_ip = True
+
+# Request that a specific image be installed on this node
+node1.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU22-64-BETA"
+node2.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU22-64-BETA"
 
 # Create a link between them
 link1 = request.Link(members = [node1,node2])
 
 # Install and execute a script that is contained in the repository.
-node1.addService(pg.Execute(shell="sh", command="/local/repository/server.sh"))
-node2.addService(pg.Execute(shell="sh", command="/local/repository/client.sh"))
+#node1.addService(pg.Execute(shell="sh", command="/local/repository/server.sh"))
+#node2.addService(pg.Execute(shell="sh", command="/local/repository/client.sh"))
+
+# Install and execute a script that is contained in the repository.
+node1.addService(pg.Execute(shell="sh", command="/local/repository/scripts/install-deps.sh"))
+node2.addService(pg.Execute(shell="sh", command="/local/repository/scripts/install-deps.sh"))
 
 # Print the RSpec to the enclosing page.
 pc.printRequestRSpec(request)
